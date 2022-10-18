da078e9f28cf3df7b4ee08d42e75ef2ed9cb1082
Vitor Lucas <vitorcrispim@yahoo.com.br>
2022-10-18 20:45:07 -0300

    Criando um segundo metodo GET e criando também um metodo que retorna por ID o anime, modificado o aplication.propeties para .yml

--- src/main/java/com/springessentials/service/AnimeService.java
+++ src/main/java/com/springessentials/service/AnimeService.java
@@ -2,16 +2,25 @@ package com.springessentials.service;
 
 import com.springessentials.models.Animes;
 import com.springessentials.repository.AnimeRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 @Service
-public class AnimeService  {
+public class AnimeService {
+    private List<Animes> animes = List.of(new Animes(1L, "Baki o Campeão"), new Animes(2L, "One Piece"));
 
-//    private final AnimeRepository animeRepository;
-    public List<Animes> listAll(){
-        return List.of(new Animes(1L,"Baki o Campeão"),new Animes(2L,"One Piece"));
+    //    private final AnimeRepository animeRepository;
+    public List<Animes> listAll() {
+        return animes;
     }
 
+    public Animes findById(long id) {
+        return animes.stream().filter(animes -> animes.getId().equals(id)).
+                findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST,"Anime não encontrado"));
+    }
 }
