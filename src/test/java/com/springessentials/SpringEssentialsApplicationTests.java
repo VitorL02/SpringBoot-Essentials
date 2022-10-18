72ba81b6f51330bb9dfcbcbc6b2aef157393252e
Vitor Lucas <vitorcrispim@yahoo.com.br>
2022-10-18 20:06:44 -0300

    Start Project

---
+++ src/test/java/com/springessentials/SpringEssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springessentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
